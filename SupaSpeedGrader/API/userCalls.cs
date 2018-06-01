fc0e9e771161d0f204421ae1a77ac482615042e4
William Brown <willbill642@yahoo.com>
2018-06-01 14:02:25 -0600

    Created function to put quiz scores for a student back

--- SupaSpeedGrader/API/userCalls.cs
+++ SupaSpeedGrader/API/userCalls.cs
@@ -8,19 +8,16 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
-using NLog;
 
 namespace SupaSpeedGrader.API
 {
     public class userCalls
     {
         /// <summary>
-		/// Implementation of Get user-in-a-course-level participation data
-		/// https://canvas.instructure.com/doc/api/analytics.html#method.analytics_api.student_in_course_participation
+		/// Implementation of Get User profile data
 		/// </summary>
 		/// <param name="accessToken"></param>
 		/// <param name="baseUrl"></param>
-		/// <param name="canvasCourseId"></param>
 		/// <param name="canvasUserId"></param>
 		/// <returns>returns a dynamic json object, will throw exception</returns>
 		public static async Task<dynamic> getUserData(string accessToken, string baseUrl, string canvasUserId)
@@ -46,5 +43,44 @@ namespace SupaSpeedGrader.API
 
             return rval;
         }
+
+        /// <summary>
+		/// Implementation of update quiz scores
+		/// Pass in a dictionary of parameters, use parameters names defined in API documnentation
+		/// </summary>
+		/// <param name="accessToken"></param>
+		/// <param name="baseUrl"></param>
+		/// <param name="courseId"></param>
+        /// <param name="quizId"></param>
+        /// <param name="studentId"></param>
+		/// <param name="vars">List of each API parameter and associated value</param>
+		/// <returns>returns a json object representing a quiz, will throw an exception</returns>
+		public static async Task<dynamic> putQuizScore(string accessToken, string baseUrl, string courseID, string quizID, string studentID, Dictionary<string, string> vars)
+        {
+            //IF this fails, check https://community.canvaslms.com/thread/6062 for some massively important JSON context information, especially regarding required parameters
+            dynamic result = null;
+            string urlCommand = "/api/v1/courses/:course_id/quizzes/:quiz_id/submissions/:id";
+
+            urlCommand = urlCommand.Replace(":course_id", courseID);
+            urlCommand = urlCommand.Replace(":quiz_id", quizID);
+            urlCommand = urlCommand.Replace(":id", studentID);
+
+            urlCommand = clsHttpMethods.concatenateHttpVars(urlCommand, vars);
+
+            using (HttpResponseMessage response = await clsHttpMethods.httpPUT(baseUrl, urlCommand, accessToken, null))
+            {
+                string rval = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode || (rval.Contains("errors") && rval.Contains("message")))
+                {
+                    throw new HttpRequestException(rval);
+                }
+                result = JsonConvert.DeserializeObject(rval);
+            }
+
+            return result;
+        }
+
+
     }
 }
