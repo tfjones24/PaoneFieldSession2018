12254eddcac732ebd1c6c9ce3d73140f15d3ef4c
Michelle Hulongbayan <mhulongbayan@mymail.mines.edu>
2018-06-11 13:36:31 -0600

    responses is now scaled correctly

--- SupaSpeedGrader/Models/GradeModel.cs
+++ SupaSpeedGrader/Models/GradeModel.cs
@@ -47,7 +47,7 @@ namespace SupaSpeedGrader.Models
         public GradeModel()
         {
             string[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-            numStudent = 5;
+            numStudent = 11;
             gradeOutOf = 20;
             names = new List<string>();
             names.Add("A");
@@ -55,16 +55,29 @@ namespace SupaSpeedGrader.Models
             names.Add("C");
             names.Add("D");
             names.Add("E");
+            names.Add("F");
+            names.Add("A1");
+            names.Add("B1");
+            names.Add("C1");
+            names.Add("D1");
+            names.Add("E1");
             string[] A = { "Alpha", "0", "Alpha" };
             string[] B = { "Beta", "0", "Beta" };
             string[] C = { "Charlie", "0", "Charlie" };
             string[] D = { "Delta", "0", "Delta" };
             string[] E = { "Echo", "0", "Echo" };
+            string[] F = {"Foxtrot","0", "Foxtrot"};
             namesGrades.Add(names[0], A);
             namesGrades.Add(names[1], B);
             namesGrades.Add(names[2], C);
             namesGrades.Add(names[3], D);
             namesGrades.Add(names[4], E);
+            namesGrades.Add(names[5], F);
+            namesGrades.Add(names[6], A);
+            namesGrades.Add(names[7], B);
+            namesGrades.Add(names[8], C);
+            namesGrades.Add(names[9], D);
+            namesGrades.Add(names[10], E);
             rubicRows = 5;
             rubicCols = 4;
             width = 100 / rubicCols;
