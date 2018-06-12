179db01f67a1be4615d0a9180050c432cacf353d
Michelle Hulongbayan <mhulongbayan@mymail.mines.edu>
2018-06-12 16:36:24 -0600

    questions now highliight

--- SupaSpeedGrader/Models/NavigationModel.cs
+++ SupaSpeedGrader/Models/NavigationModel.cs
@@ -13,11 +13,13 @@ namespace SupaSpeedGrader.Models
 
         public List<string> quizzes = new List<string>();
         public Dictionary<string, string> quizID = new Dictionary<string, string>();
+        public Dictionary<string, string> quizColor = new Dictionary<string, string>();
 
         public Dictionary<string, List<string>> quizdata = new Dictionary<string, List<string>>();
         public List<string> rubic = new List<string>();
         //Garbage to ignore, only for hardcoded bullshit
         public List<string> questions = new List<string>();
+        public Dictionary<string, string> questionColor = new Dictionary<string, string>();
 
         public NavigationModel()
         {
@@ -50,7 +52,15 @@ namespace SupaSpeedGrader.Models
             rubic.Add("A");
             rubic.Add("B");
             rubic.Add("C");
+            
+            questionColor.Add(questions[0], "started");
+            questionColor.Add(questions[1], "done");
+            questionColor.Add(questions[2], "unstarted");
+            quizColor.Add(quizzes[0],"started");
+            quizColor.Add(quizzes[1],"done");
+            quizColor.Add(quizzes[2], "unstarted");
             updateQuizData();
+
         }
 
     
