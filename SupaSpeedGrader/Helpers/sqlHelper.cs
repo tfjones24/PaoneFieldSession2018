2c1bbf3e109b208bd099c2901c507374c2d6ed26
William Brown <willbill642@yahoo.com>
2018-06-11 22:14:15 -0600

    Added extra helper classes, SQL scripts for database

---
+++ SupaSpeedGrader/Helpers/sqlHelper.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace oauth2Test.Helpers
+{
+	public class sqlHelper
+	{
+		private static string _camsConnectionString = (System.Configuration.ConfigurationManager.ConnectionStrings["oauth2"] != null) ? System.Configuration.ConfigurationManager.ConnectionStrings["oauth2"].ToString() : string.Empty;
+
+		public static string getStateJson(Guid stateId)
+		{
+			string rval = string.Empty;
+
+			string sql = "select statejson from statecache where stateId = '" + stateId.ToString() + "'";
+			using (SqlConnection dbcon = new SqlConnection(_camsConnectionString))
+			{
+				dbcon.Open();
+				using (SqlCommand cmd = new SqlCommand("dbo.getStateJson", dbcon))
+				{
+					cmd.CommandType = CommandType.Text;
+					cmd.CommandText = sql;
+
+					DataSet ds = new DataSet();
+					SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+					adapter.Fill(ds);
+
+					if (ds != null && ds.Tables[0].Rows.Count == 1)
+					{
+						rval = ds.Tables[0].Rows[0][0].ToString();
+					}
+				}
+			}
+
+			return rval;
+		}
+
+		public static bool storeState(Guid stateId, string json)
+		{
+			bool rval = true;
+
+			string sql = string.Empty;
+			string stateJson = getStateJson(stateId);
+			using (SqlConnection dbcon = new SqlConnection(_camsConnectionString))
+			{
+				dbcon.Open();
+				using (SqlCommand cmd = new SqlCommand("dbo.storeStateJson", dbcon))
+				{
+					cmd.CommandType = CommandType.Text;
+
+					if(string.IsNullOrEmpty(stateJson))
+					{
+						sql = string.Format("insert into statecache (stateid, statejson) values ('{0}', '{1}')", stateId.ToString(), json);
+					}
+					else
+					{
+						sql = string.Format("update statecache set statejson = '{0}' where stateid = '{1}'", json, stateId.ToString());
+					}
+					cmd.CommandText = sql;
+					cmd.ExecuteNonQuery();
+				}
+			}
+
+			return rval;
+		}
+
+		public static userAccessToken getUserAccessToken(long userId)
+		{
+			userAccessToken rval = null;
+
+			string sql = "select * from accessTokenCache where userid = '" + userId.ToString() + "'";
+			using (SqlConnection dbcon = new SqlConnection(_camsConnectionString))
+			{
+				dbcon.Open();
+				using (SqlCommand cmd = new SqlCommand("dbo.getUserAccessToken", dbcon))
+				{
+					cmd.CommandType = CommandType.Text;
+					cmd.CommandText = sql;
+
+					DataSet ds = new DataSet();
+					SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+					adapter.Fill(ds);
+
+					if (ds != null && ds.Tables[0].Rows.Count == 1)
+					{
+						//we found an existing token, return the token helper object
+						rval = new userAccessToken(ds.Tables[0].Rows[0]);
+					}
+				}
+			}
+
+			return rval;
+		}
+
+		public static bool storeUserAccessToken(userAccessToken accessToken)
+		{
+			bool rval = true;
+
+			string sql = string.Empty;
+			userAccessToken cacheToken = getUserAccessToken(accessToken.userId);
+			using (SqlConnection dbcon = new SqlConnection(_camsConnectionString))
+			{
+				dbcon.Open();
+				using (SqlCommand cmd = new SqlCommand("dbo.storeUserAccessToken", dbcon))
+				{
+					cmd.CommandType = CommandType.Text;
+
+					if (cacheToken == null)
+					{
+						sql = string.Format("insert into accesstokencache (userid, accesstoken, refreshtoken, tokenlife, tstamp, responseUrl) values ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}')", accessToken.userId, accessToken.accessToken, accessToken.refreshToken, accessToken.tokenLife, accessToken.tstamp.ToString(), accessToken.responseUrl);
+					}
+					else
+					{
+						sql = string.Format("update accesstokencache set accesstoken='{0}', tokenlife='{1}', tstamp='{2}', responseUrl='{4}' where userid = '{3}'", accessToken.accessToken, accessToken.tokenLife, accessToken.tstamp.ToString(), accessToken.userId, accessToken.responseUrl);
+					}
+					cmd.CommandText = sql;
+					cmd.ExecuteNonQuery();
+				}
+			}
+
+			return rval;
+		}
+
+		public static bool deleteUserAccessToken(string userId)
+		{
+			bool rval = true;
+
+			string sql = string.Empty;
+			using (SqlConnection dbcon = new SqlConnection(_camsConnectionString))
+			{
+				dbcon.Open();
+				using (SqlCommand cmd = new SqlCommand("dbo.deleteUserAccessToken", dbcon))
+				{
+					cmd.CommandType = CommandType.Text;
+					sql = string.Format("delete accesstokencache where userid = '" + userId + "'");
+					cmd.CommandText = sql;
+					cmd.ExecuteNonQuery();
+				}
+			}
+
+			return rval;
+		}
+
+	}
+}
---
+++ SupaSpeedGrader/Helpers/userAccessToken.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace oauth2Test.Helpers
+{
+	/// <summary>
+	/// Use thie helper class to simplify serialization/deserialization of the token data
+	/// </summary>
+	public class userAccessToken
+	{
+		public long userId = 0;
+		public string accessToken = string.Empty;
+		public string refreshToken = string.Empty;
+		public int tokenLife = 0;
+		public DateTime tstamp = DateTime.Now;
+		public string responseUrl = string.Empty;
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public userAccessToken()
+		{
+		}
+
+		/// <summary>
+		/// Constructor to handle DataRow from cache db
+		/// </summary>
+		/// <param name="dr">DataRow from the cache database</param>
+		public userAccessToken(DataRow dr)
+		{
+			long.TryParse(dr["userId"].ToString(), out userId);
+			accessToken = dr["accessToken"].ToString();
+			refreshToken = dr["refreshToken"].ToString();
+			int.TryParse(dr["tokenLife"].ToString(), out tokenLife);
+			DateTime.TryParse(dr["tstamp"].ToString(), out tstamp);
+			responseUrl = dr["responseUrl"].ToString();
+		}
+
+		/// <summary>
+		/// Process the json blob returned by Canvas to retrieve the user token values
+		/// </summary>
+		/// <param name="json"></param>
+		public userAccessToken(string json, string responseUrl)
+		{
+			dynamic obj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+			if (obj.user != null && obj.user.id != null) long.TryParse(obj.user.id.ToString(), out userId);
+			if (obj.access_token != null) accessToken = obj.access_token.ToString();
+			if (obj.refresh_token != null) refreshToken = obj.refresh_token.ToString();
+			if (obj.expires_in != null) int.TryParse(obj.expires_in.ToString(), out tokenLife);
+			tstamp = DateTime.Now;
+			this.responseUrl = responseUrl;
+			sqlHelper.storeUserAccessToken(this);
+		}
+
+		/// <summary>
+		/// Our Canvas token is only good for [tokenLive] seconds
+		/// Calculate the time since we last refreshed.
+		/// </summary>
+		public bool tokenRefreshRequired
+		{
+			get
+			{
+				bool rval = false;
+
+				if (refreshToken == null)
+					return true;
+
+				if (tstamp < DateTime.Now.AddSeconds(-1 * tokenLife))
+					return true;
+
+				return rval;
+			}
+		}
+
+	}
+}
