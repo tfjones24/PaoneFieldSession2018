98cc47c66f0b06b550c9349d1877070ef0d3ae25
William Brown <willbill642@yahoo.com>
2018-06-13 16:25:39 -0600

    Flushing out the nav page

--- SupaSpeedGrader/Controllers/apiController.cs
+++ SupaSpeedGrader/Controllers/apiController.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json.Linq;
+using NLog;
 using SupaSpeedGrader.API;
 using SupaSpeedGrader.Helpers;
 using SupaSpeedGrader.Models;
@@ -14,9 +15,43 @@ namespace SupaSpeedGrader.Controllers
 {
     public class apiController : Controller
     {
-        public JsonResult getQuestions(string quiz)
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public async Task<JsonResult> getQuestionsAsync(string quiz, string state)
         {
-            return Json(new { Result = String.Format("Fist item in list: '{0}'", quiz) });
+            oauthHelper oauth = Newtonsoft.Json.JsonConvert.DeserializeObject<oauthHelper>(sqlHelper.getStateJson(Guid.Parse(state)));
+
+            oauth.accessToken = sqlHelper.getUserAccessToken(long.Parse(oauth.custom_canvas_user_id));
+
+            if (oauth.accessToken != null)
+            {
+
+                //now validate the token to make sure it's still good
+                //if the token is no good try to refresh it
+                if (oauth.accessToken.tokenRefreshRequired)
+                {
+                    if (await userCalls.requestUserToken(oauth, oauth.accessToken.responseUrl, "refresh_token", null, oauth.accessToken.refreshToken) == false)
+                    {
+                        /***********************************************************/
+                        //	If we're here it the user may have deleted the access token in their profile.
+                        //  In this case we will request a brand new token, forcing the user to "Authorize" again.
+                        //  To test this, delete the token in your Canvas user profile.
+                        /***********************************************************/
+
+                        return Json(new { Result = "AUTHFAIL" });
+
+                    }
+
+                }
+
+
+            }
+            _logger.Error("Calling for questions");
+            JArray questions = await userCalls.getListQuestionsInQuiz(oauth.accessToken.accessToken, "https://" + oauth.host, oauth.custom_canvas_course_id, quiz);
+
+            _logger.Error("Call for questions returned:" + questions.ToString());
+
+            return Json(new { Result = "SUCCESS", Extra = questions.ToString() });
         }
     }
 }
