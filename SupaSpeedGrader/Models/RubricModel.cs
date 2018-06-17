04a9078f7b8908a59f0e468d40bfa5bb4985cfc6
Michelle Hulongbayan <mhulongbayan@mymail.mines.edu>
2018-06-16 19:56:36 -0600

    rubric varaibles commented and renamed

--- SupaSpeedGrader/Models/RubricModel.cs
+++ SupaSpeedGrader/Models/RubricModel.cs
@@ -10,23 +10,13 @@ namespace SupaSpeedGrader.Models
     {
         // GET: RubicModel
         public List<string> questions = new List<string>();
-        public int numWidth;
 
         public RubricModel()
         {
-            addHardValue();
         }
         public ActionResult Index()
         {
             return View();
-        }
-        public void addHardValue()
-        {
-            for (int x = 1; x < 11; ++x)
-            {
-                questions.Add(x.ToString());
-            }
-            numWidth = 100 / (questions.Count + 1);
         }
     }
 }
